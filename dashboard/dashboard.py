7ebe45b0348e82ca575e08a4d19982a97f1aa3f5
shafira <124395825+itspiroy@users.noreply.github.com>
2025-03-18 18:50:32 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -42,17 +42,26 @@ day_df = day_df[(day_df['dteday'] >= pd.to_datetime(start_date)) & (day_df['dted
 day_df['season'] = day_df['season'].map({1: 'Spring', 2: 'Summer', 3: 'Fall', 4: 'Winter'})
 day_df = day_df[day_df['season'].isin(selected_season)]
 
-# 1. Visualisasi distribusi penyewaan sepeda pada setiap musim
-st.subheader("Bagaimana distribusi penyewaan sepeda pada setiap musim?")
-season_avg = day_df.groupby('season')['cnt'].mean().reset_index()
-plt.figure(figsize=(8, 5))
-sns.barplot(x='season', y='cnt', data=season_avg, palette=['#ff9999', '#66b3ff', '#99ff99', '#ffcc99'])
-plt.title('Distribusi Penyewaan Sepeda pada Setiap Musim')
-plt.xlabel('Musim')
-plt.ylabel('Rata-rata Jumlah Penggunaan Sepeda')
-st.pyplot(plt)
+# 1. Bagaimana pengaruh kondisi cuaca terhadap jumlah penggunaan sepeda pada berbagai jam dalam sehari?
+sns.set(style="whitegrid")
 
-# 2. Visualisasi distribusi penyewaan sepeda berdasarkan tipe penyewa
+weather_avg = hour_df.groupby(['hr', 'weathersit'])['cnt'].mean().reset_index()
+
+weather_avg['weathersit'] = weather_avg['weathersit'].map({
+    1: 'Clear',
+    2: 'Cloudy',
+    3: 'Rain',
+    4: 'Severe'
+})
+plt.ylabel('Jumlah Penggunaan Sepeda')
+plt.xticks(range(0, 24))  
+plt.legend(title='Cuaca')
+
+plt.tight_layout()
+
+plt.show()
+
+# 2. Bagaimana distribusi penyewaan sepeda berdasarkan tipe penyewa?
 st.subheader("Bagaimana distribusi penyewaan sepeda berdasarkan tipe penyewa?")
 hour_df['user_type'] = hour_df['registered'].apply(lambda x: 'Registered' if x > 0 else 'Casual')
 user_type_avg = hour_df.groupby(['hr', 'user_type'])['cnt'].mean().reset_index()
